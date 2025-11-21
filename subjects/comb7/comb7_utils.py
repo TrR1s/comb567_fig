d5d12cd70fff91e126665e191a51233f69b52399
TrR1s <anatolyturin0505@gmail.com>
2025-11-21 12:30:54 +0300

    count amount

--- subjects/comb7/comb7_utils.py
+++ subjects/comb7/comb7_utils.py
@@ -1,6 +1,9 @@
+import numpy as np
+import math
 from itertools import combinations
 # from subjects.comb7.comb_7_list_wo_suit import do_comb7_with_fl_list
 from subjects.comb5.comb5_simple import Comb5Simple
+from subjects import Deck
 
 class Comb7Tools():
     
@@ -30,3 +33,57 @@ class Comb7Tools():
             'best_name': best_name,
             'str_key': best_str_key_5
         }             
+    
+    def do_str_key(comb7_tuple:tuple[list[int], list[int]]) -> str:
+        rank_list, flush_list = comb7_tuple
+        rank_list_str = ','.join(map(str, sorted(rank_list)))
+        flush_str = 'unfl'
+        if flush_list != []:
+            flush_str = ','.join(map(str, sorted(flush_list)))
+        
+        return f'{rank_list_str}_{flush_str}'
+    
+    def count_amount_in_deck(comb7_tuple:tuple[list[int], list[int]],deck: Deck) -> int:
+        rank_list, flush_list = comb7_tuple
+        
+        
+        
+        for curr_fl_card in flush_list:
+           rank_list.remove(curr_fl_card) 
+        
+        
+        # count flash amount
+        flush_am = 0
+        flush_list = np.array(flush_list)
+        flush_list -=2
+        for suit_i in range(4):
+            fl_can = True
+            for curr_rank in flush_list:
+                if deck.suits_0_1[suit_i][curr_rank] == 0:
+                    fl_can = False
+                    break
+            if fl_can:
+                flush_am +=1
+                    
+        rank_amount_wo_flset = deck.rank_amount[:]
+        
+        if flush_list.size > 0:
+            if flush_am == 0 : return 0
+        else:
+            flush_am = 1
+        
+        for curr_rank in flush_list:
+            rank_amount_wo_flset[curr_rank] -=1
+        
+        
+        rank_list_np = np.array(rank_list)
+        rank, freq = np.unique(rank_list_np, return_counts=True)
+        rank -=2
+        total_amount = 1
+        for i,cur_rank in enumerate(rank):
+            curr_amount = freq[i]
+            
+            total_amount *= math.comb(rank_amount_wo_flset[cur_rank],curr_amount)
+        
+            
+        return total_amount*flush_am
--- test_comb7.py
+++ test_comb7.py
@@ -1,4 +1,9 @@
 from subjects import Comb7Tools
+from subjects import Deck,DeckTools
+deck = DeckTools.do_full_deck()
 
-comb7_tuple = ([8, 9, 10, 11, 12, 13, 14], [8, 9, 10, 11, 12,  14])
+comb7_tuple = ([10, 10, 10, 11, 12, 13, 14], [10, 11, 12, 13, 14])
 print(Comb7Tools.find_best_comb5(comb7_tuple))
+print(Comb7Tools.do_str_key(comb7_tuple))
+
+print(Comb7Tools.count_amount_in_deck(comb7_tuple,deck))
